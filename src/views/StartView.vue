5996a6c38d61aeede0860849234e3fcdb041f52f
ShiniYanada <ysthon@gmail.com>
2019-06-16 11:28:23 +0900

    テストの問題数を決める画面を作成

---
+++ src/views/StartView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="start">
+        <h1>現在の単語数は{{ $store.state.cards.length }}個です</h1>
+        <p>問題数を何問にしますか？</p>
+        <el-input-number v-model="num" :min="1" :max="10"></el-input-number> <br/>
+        <el-button type="success" round　v-on:click="setQuizeNumber">決定</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            num: 1,
+        }
+    },
+    computed: {
+        max(){
+            return this.$store.state.cards.length
+        }
+    },
+    methods: {
+        setQuizeNumber(){
+            console.log(this.num)
+        }
+    }
+}
+</script>
+
